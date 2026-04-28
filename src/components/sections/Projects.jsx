a5d4a70b051ae5e6a2345367d230168090f2f096
RajaRam2405 <muthuraja240405@gmail.com>
2026-04-28 16:04:38 +0530

    fix: Remove AnimatePresence mode to restore project card rendering

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -277,7 +277,7 @@ export default function Projects() {
         className="grid md:grid-cols-2 lg:grid-cols-3 gap-6"
         layout
       >
-        <AnimatePresence mode="wait">
+        <AnimatePresence>
           {filtered.map((project) => (
             <ProjectCard
               key={project.id}
